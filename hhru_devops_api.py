3afe5462afad5152b3043882ec860139e2b16dee
Atstec_laptop <you@example.com>
2023-01-02 00:00:27 +0500

    add devops_2

--- hhru_devops_api.py
+++ hhru_devops_api.py
@@ -6,7 +6,7 @@ import requests
 
 def main():
     for date in pd.date_range(start='2022-12-01', end='2022-12-31', freq='D'):
-        crete_csv('devops.cvs', date)
+        crete_csv(f'devops_{date}.cvs', date)
 
 
 def crete_csv(name, date):
@@ -21,21 +21,18 @@ def crete_csv(name, date):
         for text in ['devops', 'development operations']:
             writer = csv.writer(f)
             writer.writerow(['name', 'salary_from', 'salary_to', 'salary_currency', 'area_name', 'published_at'])
-            for hour in range(0, 22):
-                for page in range(0, 20):
-                    vacancies = get_page(page, f'{f"0{hour}" if hour < 10 else hour}:00:00',
-                                         f'{f"0{hour + 1}" if hour + 1 < 10 else hour + 1}:00:00', date,text)
-                    if vacancies.__contains__('items'):
-                        for row in vacancies['items']:
-                            if row['salary'] is None:
-                                writer.writerow([row['name'], None, None, None, row['area']['name'], row['published_at']])
-                            else:
-                                writer.writerow([row['name'], row['salary']['from'], row['salary']['to'],
-                                                 row['salary']['currency'], row['area']['name'], row['published_at']])
-                    else:
-                        print(f'{f"0{hour}" if hour < 10 else hour}:00:00' jf'{f"0{hour + 1}" if hour + 1 < 10 else hour + 1}:00:00')
-                        print(vacancies) # для ошибок получении api запроса
-                    time.sleep(0.25)
+            for page in range(0, 20):
+                vacancies = get_page(1, '00:00:00', '23:59:59', date, text)
+                if vacancies.__contains__('items'):
+                    for row in vacancies['items']:
+                        if row['salary'] is None:
+                            writer.writerow([row['name'], None, None, None, row['area']['name'], row['published_at']])
+                        else:
+                            writer.writerow([row['name'], row['salary']['from'], row['salary']['to'],
+                                             row['salary']['currency'], row['area']['name'], row['published_at']])
+                else:
+                    print(vacancies)  # для ошибок получении api запроса
+                time.sleep(0.25)
 
 
 def get_page(page, time_from, time_to, date, text):
@@ -55,7 +52,7 @@ def get_page(page, time_from, time_to, date, text):
         'page': page,
         'per_page': 100,
         'date_from': f'{date}T{time_from}+0300',
-        'date_to': f'{date}T{time_to}+0300',
+       # 'date_to': f'{date}T{time_to}+0300',
     }
     request = requests.get('https://api.hh.ru/vacancies', params).json()
     return request
