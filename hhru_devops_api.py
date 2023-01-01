4b6f4e30358e22b69b0756856c349ae4f55496b8
Atstec_laptop <you@example.com>
2023-01-01 22:45:52 +0500

    add devops

---
+++ hhru_devops_api.py
@@ -0,0 +1,66 @@
+import csv
+import time
+
+import pandas as pd
+import requests
+
+def main():
+    for date in pd.date_range(start='2022-12-01', end='2022-12-31', freq='D'):
+        crete_csv('devops.cvs', date)
+
+
+def crete_csv(name, date):
+    """
+    Создает csv файл по данным из hh.ru api
+    Args:
+        name(str): Имя файла
+        date(str): дата для запроса
+    :return:
+    """
+    with open(f"{name}", "w", newline="", encoding='utf-8-sig') as f:
+        for text in ['devops', 'development operations']:
+            writer = csv.writer(f)
+            writer.writerow(['name', 'salary_from', 'salary_to', 'salary_currency', 'area_name', 'published_at'])
+            for hour in range(0, 22):
+                for page in range(0, 20):
+                    vacancies = get_page(page, f'{f"0{hour}" if hour < 10 else hour}:00:00',
+                                         f'{f"0{hour + 1}" if hour + 1 < 10 else hour + 1}:00:00', date,text)
+                    if vacancies.__contains__('items'):
+                        for row in vacancies['items']:
+                            if row['salary'] is None:
+                                writer.writerow([row['name'], None, None, None, row['area']['name'], row['published_at']])
+                            else:
+                                writer.writerow([row['name'], row['salary']['from'], row['salary']['to'],
+                                                 row['salary']['currency'], row['area']['name'], row['published_at']])
+                    else:
+                        print(f'{f"0{hour}" if hour < 10 else hour}:00:00' jf'{f"0{hour + 1}" if hour + 1 < 10 else hour + 1}:00:00')
+                        print(vacancies) # для ошибок получении api запроса
+                    time.sleep(0.25)
+
+
+def get_page(page, time_from, time_to, date, text):
+    """
+    Отправляет api запрос на hh.ru
+    Arguments
+        page: Номер страницы, с которой приходит вакансия (int)
+        time_from: Начало временного промежутка, с которого начинается выбор вакансии (str)
+        time_to: Конец временного промежутка, на котором заканчивается выбор вакансии (str)
+        date: Дата, когда была опубликована вакансия (str)
+    Returns:
+        list: список вакансий в формате json
+    """
+    params = {
+        'text': text,
+        'specialization': 1,
+        'page': page,
+        'per_page': 100,
+        'date_from': f'{date}T{time_from}+0300',
+        'date_to': f'{date}T{time_to}+0300',
+    }
+    request = requests.get('https://api.hh.ru/vacancies', params).json()
+    return request
+
+
+
+if __name__ == '__main__':
+    main()
